afcfd3167ce91a20878b088a13b5d806c330e967
Prasun <prasun.hazra@gmail.com>
2020-12-08 18:28:00 +0530

    Initial building blocks

---
+++ PromotionEngine/PromotionEngine/SKU.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    interface ISKU
+    {
+        int Cost();
+    }
+
+    abstract class PromotionSKU : ISKU
+    {
+        private List<ISKU> _lSku;
+        public PromotionSKU(List<ISKU> lsku)
+        {
+            _lSku = lsku;
+        }
+        public abstract int Cost();
+    }
+}
